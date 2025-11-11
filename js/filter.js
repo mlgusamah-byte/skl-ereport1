b1f4a1b33a2415d841fce210b8637aa3732bb14a
Usamah Awais Al-Qarni <mlgusamah@gmail.com>
2025-11-11 14:26:59 +0700

    edit semua

--- js/filter.js
+++ js/filter.js
@@ -2,7 +2,7 @@ const laporanList = JSON.parse(localStorage.getItem('laporanList')) || [];
 const filterInput = document.getElementById('filterInput');
 const tbody = document.querySelector('#hasilFilter tbody');
 
-filterInput.addEventListener('keyup', function() {
+filterInput.addEventListener('keyup', function () {
     const keyword = filterInput.value.toLowerCase();
     tbody.innerHTML = '';
 
@@ -12,20 +12,19 @@ filterInput.addEventListener('keyup', function() {
         tbody.innerHTML = '<tr><td colspan="7">Tidak ada data ditemukan</td></tr>';
     } else {
         hasil.forEach((lap, i) => {
-            const status = lap.nilai >= 75 ? 'Lulus' : 'Tidak Lulus';
-            const statusClass = lap.nilai >= 75 ? 'status-lulus' : 'status-gagal';
+            const status = lap.nilai >= 75 ? 'Lulus' : 'Tidak lulus';
+            const statusClass = lap.nilai >= 75 ? 'status-lulus' : 'status-tidak';
 
-            tbody.innerHTML += `
-                <tr>
-                    <td>${i + 1}</td>
-                    <td>${lap.nama}</td>
-                    <td>${lap.kelas}</td>
-                    <td>${lap.judul}</td>
-                    <td>${lap.nilai}</td>
-                    <td class="${statusClass}">${status}</td>
-                    <td>${lap.tanggal}</td>
-                </tr>
-            `;
+            tbody.innerHTML += `<tr>
+                <td>${i + 1}</td>
+                <td>${lap.nama}</td>
+                <td>${lap.kelas}</td>
+                <td>${lap.judul}</td>
+                <td>${lap.nilai}</td>
+                <td class="${statusClass}">${status}</td>
+                <td>${lap.tanggal}</td>
+            </tr>`;
         });
     }
 });
+
